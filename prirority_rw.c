57155b3506824783e4c5cd4380f56bdc7e045715
Aryan <ether9095cable@gmail.com>
2025-11-07 17:14:23 +0530

    concurrent

--- prirority_rw.c
+++ prirority_rw.c
@@ -5,14 +5,14 @@
 
 int cnt = 0;
 pthread_mutex_t write_mutex;
-pthread_mutex_t priority;
+pthread_mutex_t queue;
 pthread_mutex_t read_mutex;
 int reader_cnt = 0;
 
 void *writer(void *args) {
   long id = (long)args;
 
-  pthread_mutex_lock(&priority);
+  pthread_mutex_lock(&queue);
 
   pthread_mutex_lock(&write_mutex);
   int old = cnt;
@@ -21,20 +21,22 @@ void *writer(void *args) {
          cnt - 1, cnt);
   pthread_mutex_unlock(&write_mutex);
 
-  pthread_mutex_unlock(&priority);
+  pthread_mutex_unlock(&queue);
   return NULL;
 }
 
 void *reader(void *args) {
   long id = (long)args;
 
-  pthread_mutex_lock(&priority);
+  pthread_mutex_lock(&queue);
 
   pthread_mutex_lock(&read_mutex);
   reader_cnt++;
   if (reader_cnt == 1)
     pthread_mutex_lock(&write_mutex);
   pthread_mutex_unlock(&read_mutex);
+
+  pthread_mutex_unlock(&queue);
 
   printf("Reader %ld: The current value of cnt is %d\n", id, cnt);
 
@@ -44,27 +46,26 @@ void *reader(void *args) {
     pthread_mutex_unlock(&write_mutex);
   pthread_mutex_unlock(&read_mutex);
 
-  pthread_mutex_unlock(&priority);
   return NULL;
 }
 
 int main() {
   pthread_mutex_init(&write_mutex, NULL);
-  pthread_mutex_init(&priority, NULL);
+  pthread_mutex_init(&queue, NULL);
   pthread_mutex_init(&read_mutex, NULL);
 
-  pthread_t readers[10], writers[15];
-  for (long i = 0; i < 10; i++)
+  pthread_t readers[10], writers[10];
+  for (long i = 0; i < 10; i++) {
     pthread_create(&readers[i], NULL, reader, (void *)i);
-  for (long i = 0; i < 15; i++)
     pthread_create(&writers[i], NULL, writer, (void *)i);
+  }
 
-  for (int i = 0; i < 10; i++)
+  for (int i = 0; i < 10; i++) {
     pthread_join(readers[i], NULL);
-  for (int i = 0; i < 15; i++)
     pthread_join(writers[i], NULL);
+  }
 
   pthread_mutex_destroy(&write_mutex);
-  pthread_mutex_destroy(&priority);
+  pthread_mutex_destroy(&queue);
   pthread_mutex_destroy(&read_mutex);
 }
--- shm_rw.c
+++ shm_rw.c
@@ -1,6 +1,7 @@
 #include <pthread.h>
 #include <semaphore.h>
 #include <stdbool.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <strings.h>
 #include <sys/ipc.h>
@@ -13,7 +14,7 @@ sem_t priority;
 int read_count = 0;
 
 void *reader(void *args) {
-  long shmid = (long)args;
+  int64_t shmid = (int64_t)args;
   sem_wait(&priority);
 
   sem_wait(&read_mutex);
@@ -22,7 +23,7 @@ void *reader(void *args) {
     sem_wait(&write_mutex);
   sem_post(&read_mutex);
 
-  long *str = (long *)shmat(shmid, NULL, 0);
+  int64_t *str = (int64_t *)shmat(shmid, NULL, 0);
   printf("The reader saw: %ld\n", *str);
   shmdt(str);
 
@@ -37,13 +38,15 @@ void *reader(void *args) {
 }
 
 void *writer(void *args) {
-  long shmid = (long)args;
+  int64_t *arg = (int64_t *)args;
+  int64_t shmid = arg[0];
+  int64_t val = arg[1];
   sem_wait(&priority);
   sem_wait(&write_mutex);
 
-  long *str = (long *)shmat(shmid, NULL, 0);
+  int64_t *str = (int64_t *)shmat(shmid, NULL, 0);
 
-  *str = 420;
+  *str = 420 + val;
   printf("writer wrote: %ld\n", *str);
   shmdt(str);
 
@@ -58,19 +61,21 @@ int main(int argc, char *argv[]) {
   sem_init(&priority, 0, 1);
 
   key_t key = ftok("shm", 69);
-  long shmid = shmget(key, 1024, 0666 | IPC_CREAT);
+  int64_t shmid = shmget(key, 1024, 0666 | IPC_CREAT);
 
-  pthread_t writers[5], readers[10];
-  for (int i = 0; i < 10; i++)
+  pthread_t writers[10], readers[10];
+  for (int64_t i = 0; i < 10; i++) {
+    int64_t arg[2];
+    arg[0] = shmid;
+    arg[1] = i;
     pthread_create(&readers[i], NULL, reader, (void *)shmid);
+    pthread_create(&writers[i], NULL, writer, arg);
+  }
 
-  for (int i = 0; i < 5; i++)
-    pthread_create(&writers[i], NULL, writer, (void *)shmid);
-
-  for (int i = 0; i < 10; i++)
+  for (int i = 0; i < 10; i++) {
     pthread_join(readers[i], NULL);
-  for (int i = 0; i < 5; i++)
     pthread_join(writers[i], NULL);
+  }
 
   sem_destroy(&read_mutex);
   sem_destroy(&write_mutex);
