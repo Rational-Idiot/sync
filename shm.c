4cb0091917601f1ac18b2c47e34b1da7f3f77bae
Aryan <ether9095cable@gmail.com>
2025-11-07 16:58:19 +0530

    shm reader writer

--- shm.c
+++ shm.c
@@ -1,3 +1,5 @@
+#include <pthread.h>
+#include <semaphore.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,22 +9,73 @@
 #include <sys/shm.h>
 #include <unistd.h>
 
+sem_t read_mutex;
+sem_t write_mutex;
+sem_t priority;
+int read_count = 0;
+
+void *reader(void *args) {
+  long shmid = (long)args;
+  sem_wait(&priority);
+
+  sem_wait(&read_mutex);
+  read_count++;
+  if (read_count == 1)
+    sem_wait(&write_mutex);
+  sem_post(&read_mutex);
+
+  sem_post(&priority);
+
+  long *str = (long *)shmat(shmid, NULL, 0);
+  printf("The reader saw: %ld\n", *str);
+  shmdt(str);
+
+  sem_wait(&read_mutex);
+  read_count--;
+  if (read_count == 0)
+    sem_post(&write_mutex);
+  sem_post(&read_mutex);
+  return NULL;
+}
+
+void *writer(void *args) {
+  long shmid = (long)args;
+  sem_wait(&priority);
+  sem_wait(&write_mutex);
+
+  long *str = (long *)shmat(shmid, NULL, 0);
+
+  *str = 420;
+  printf("writer wrote: %ld\n", *str);
+  shmdt(str);
+
+  sem_post(&write_mutex);
+  sem_post(&priority);
+  return NULL;
+}
+
 int main(int argc, char *argv[]) {
-  int pid = fork();
-  if (pid == 0) {
-    key_t key = ftok("shm", 69);
-    long shmid = shmget(key, 1024, 0666);
+  sem_init(&read_mutex, 0, 1);
+  sem_init(&write_mutex, 0, 1);
+  sem_init(&priority, 0, 1);
 
-    long *str = (long *)shmat(shmid, NULL, 0);
-    printf("\nThe reader saw: %ld", *str);
-    shmdt(str);
-  } else {
-    key_t key = ftok("shm", 69);
-    int shmid = shmget(key, 1024, 0666 | IPC_CREAT);
-    long *str = (long *)shmat(shmid, NULL, 0);
+  key_t key = ftok("shm", 69);
+  long shmid = shmget(key, 1024, 0666 | IPC_CREAT);
 
-    *str = 420;
-    printf("writer wrote: %d", *str);
-    shmdt(str);
-  }
+  pthread_t writers[5], readers[10];
+  for (int i = 0; i < 10; i++)
+    pthread_create(&readers[i], NULL, reader, (void *)shmid);
+
+  for (int i = 0; i < 5; i++)
+    pthread_create(&writers[i], NULL, writer, (void *)shmid);
+
+  for (int i = 0; i < 10; i++)
+    pthread_join(readers[i], NULL);
+  for (int i = 0; i < 5; i++)
+    pthread_join(writers[i], NULL);
+
+  sem_destroy(&read_mutex);
+  sem_destroy(&write_mutex);
+  sem_destroy(&priority);
+  shmctl(shmid, IPC_RMID, NULL);
 }
